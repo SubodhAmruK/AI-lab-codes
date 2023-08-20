aadf811dbd2fb326641738a86482a82284b944d4
SubodhAmruK <subodhamru2003@gmail.com>
2023-08-20 15:16:42 +0530

    New

--- even_degrees.py
+++ even_degrees.py
@@ -1,23 +1,29 @@
-n = int(input("Enter no.of vertices : "))
-m = int(input("Enter no.of Edges : "))
-degrees = [0]*n
-graph = [[] for _ in range(n)]
+n,m = map(int,input("Enter the values of N and M : ").split())
+u,v=0,0
+degree = [0]*(n)
+graph = [-1]*m
+print(degree)
+print(graph)
 
-if(m%2 != 0):
-    print("-1")
+if(m%2!=0 and n%2!=0):
+    print(-1)
 else:
     for i in range(m):
-        print("Enter connection between nodes. Edge {} : ".format(i+1))
-        u,v = map(int,input().split())
-        u -= 1
-        v -= 1
-        if (u == v):
-            print("No self loops allowed")
-            break
-        else:
-            graph[u].append(v)
-            degrees[u] +=1
-            degrees[v] +=1
-
-print(degrees)
-print(graph)
+        u,v = map(int,input("Enter the values of vertices to connect : ").split())
+        if degree[u-1] %2 == 0 and degree[v-1] %2 == 0:
+            graph[i] = 0;
+            degree[v-1]+=1
+        elif degree[u-1] %2 != 0 and degree[v-1] %2 == 0:
+            graph[i] = 1;
+            degree[u-1]+=1
+        elif degree[u-1] %2 == 0 and degree[v-1] %2 != 0:
+            degree[v-1]+=1    
+            graph[i] = 1;
+        elif degree[u-1] %2 != 0 and degree[v-1] %2 != 0:
+            degree[v-1]+=1 
+    for i in degree:
+        if i%2 !=0:
+            print(-1)
+            exit(0);
+    
+    print(graph)
