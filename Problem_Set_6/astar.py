e81a77fcfd0f0f125f6cc59c79c2bca5656e3072
SubodhAmruK <subodhamru2003@gmail.com>
2023-09-19 09:45:24 +0530

    neu

--- Problem_Set_6/astar.py
+++ Problem_Set_6/astar.py
@@ -2,12 +2,12 @@ import copy
 
 graph = [[8, 3, 4], [1, 7, 2], [5, 6, 0]]  # initial state
 goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]  # final state
-level = 4  # no of levels
-queue = [] # queue
-n = 2 # size of matrix
-g=0 # calculates g(n)
+#level = 9999999999999999  # no of levels
+queue = []
+n = 2
+g=-1
 
-def print_graph(graph): 
+def print_graph(graph):
     for row in graph:
         for col in row:
             print(col, end=" ")
@@ -34,124 +34,100 @@ def right(graph, row_index, col_index):
         return True
     return False
 
-def find_row_index(matrix):
+def find_row_index(matrix,val):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            if matrix[i][j] == 0:
+            if matrix[i][j] == val:
                 return i
     return -1
 
-def find_col_index(matrix):
-    for row in matrix:
-        for j in range(len(row)):
-            if row[j] == 0:
+def find_col_index(matrix,val):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] == val:
                 return j
     return -1
 
 def find_heu(graph):
-    i = 0
-    hs = 0
-    for i in range(3):
-        for j in range(3):
-            if graph[i][j] != goal_state[i][j]:
-                hs += 1
+    hs=-1
+    for i in range(len(graph)):
+        for j in range(len(graph[0])):
+            row = find_row_index(goal_state,graph[i][j])
+            col = find_col_index(goal_state,graph[i][j])
+            hs += abs(row-i)+abs(col-j)
+    
     return hs
-def next_state(graph,next_hs):
-    hs = find_heu(graph)
-    fs = hs+g
-    if next_hs==-1:
-        next_hs = fs
-    elif fs<=next_hs:
-        next_hs = fs
-        next_trav = copy.deepcopy(graph)
+def find_least_hs(queue):
+    hs = 500
+    g = 0
+    for q in queue:
+        if find_heu(q) <= hs:
+            hs = find_heu(q)
+            g = q
+    print("HS in Func",hs)
+    return g
 
 def create(g):
-    row_index = find_row_index(graph)
-    col_index = find_col_index(graph)
+    i=0
     queue.append(copy.deepcopy(graph))
-    print_graph(queue[0])
-    for i in range(level):
-        next_hs = -1
-        next_trav = 0
-        print("Level : ", (i + 1))
+    #print_graph(queue[0])
+    while goal_state != queue[0]:
+        
+        print("Level : ", (i))
         g+=1
         num_nodes_at_level = len(queue)
-        for j in range(num_nodes_at_level):
-            current_state = queue.pop(0)
-            row_index = find_row_index(current_state)
-            col_index = find_col_index(current_state)
-            if up(current_state, row_index, col_index):
-                temp = current_state[row_index][col_index]
-                current_state[row_index][col_index] = current_state[row_index - 1][col_index]
-                current_state[row_index - 1][col_index] = temp
-                hs = find_heu(current_state)
-                fs = hs+g
-                next_state(current_state,next_hs)
-                #print("H(s) : ",hs)
-                #print("G(s) : ",g)
-                #print("F(s) : ",hs+g)
-                #print()
-                #print_graph(current_state)
-                #queue.append(copy.deepcopy(current_state))
-                temp = current_state[row_index][col_index]
-                current_state[row_index][col_index] = current_state[row_index - 1][col_index]
-                current_state[row_index - 1][col_index] = temp
-            
-            if left(current_state, row_index, col_index):
-                temp = current_state[row_index][col_index]
-                current_state[row_index][col_index] = current_state[row_index][col_index - 1]
-                current_state[row_index][col_index - 1] = temp
-                hs = find_heu(current_state)
-                fs = hs+g
-                next_state(current_state,next_hs)
-                #print("H(s) : ",hs)
-                #print("G(s) : ",g)
-                #print("F(s) : ",fs)
-                #print()
-                #print_graph(current_state)
-                #queue.append(copy.deepcopy(current_state))
-                temp = current_state[row_index][col_index]
-                current_state[row_index][col_index] = current_state[row_index][col_index - 1]
-                current_state[row_index][col_index - 1] = temp
-            if down(current_state, row_index, col_index):
-                temp = current_state[row_index][col_index]
-                current_state[row_index][col_index] = current_state[row_index + 1][col_index]
-                current_state[row_index + 1][col_index] = temp
-                hs = find_heu(current_state)
-                fs=hs+g
-                next_state(current_state,next_hs)
-                #print("H(s) : ",hs)
-                #print("G(s) : ",g)
-                #print("F(s) : ",fs)
-                #print()
-                #print_graph(current_state)
-                #queue.append(copy.deepcopy(current_state))
-                temp = current_state[row_index][col_index]
-                current_state[row_index][col_index] = current_state[row_index + 1][col_index]
-                current_state[row_index + 1][col_index] = temp
-            if right(current_state, row_index, col_index):
-                temp = current_state[row_index][col_index]
-                current_state[row_index][col_index] = current_state[row_index][col_index + 1]
-                current_state[row_index][col_index + 1] = temp
-                hs = find_heu(current_state)
-                fs = hs+g
-                next_state(current_state,next_hs)
-                #print("H(s) : ",hs)
-                #print("G(s) : ",g)
-                #print("F(s) : ",hs+g)
-                #print()
-                #print_graph(current_state)
-                #queue.append(copy.deepcopy(current_state))
-                temp = current_state[row_index][col_index]
-                current_state[row_index][col_index] = current_state[row_index][col_index + 1]
-                current_state[row_index][col_index + 1] = temp
-        hs = find_heu(next_trav)
+        print("Number of nodes a each level : ",num_nodes_at_level)
+        next_node_to_select = find_least_hs(queue)
+        queue.clear() # This will clear the contents of my_list
+        hs = find_heu(next_node_to_select)
         fs = hs+g
-        print("H(s) : ",hs)
-        print("G(s) : ",g)
-        print("F(s) : ",fs)
+        print("H(s):",hs,"  G(s):",g,"  F(s):",fs)
         print()
-        queue.append(copy.deepcopy(next_trav))
-        print(next_trav)
+        print_graph(next_node_to_select)
+        
+        current_state = next_node_to_select
+        row_index = find_row_index(current_state,0)
+        col_index = find_col_index(current_state,0)
+        
+        if up(current_state, row_index, col_index):
+            temp = current_state[row_index][col_index]  # type: ignore
+            current_state[row_index][col_index] = current_state[row_index - 1][col_index] # type: ignore
+            current_state[row_index - 1][col_index] = temp # type: ignore
+            
+            queue.append(copy.deepcopy(current_state))
+            temp = current_state[row_index][col_index] # type: ignore
+            current_state[row_index][col_index] = current_state[row_index - 1][col_index] # type: ignore
+            current_state[row_index - 1][col_index] = temp # type: ignore
+        
+        if left(current_state, row_index, col_index):
+            temp = current_state[row_index][col_index] # type: ignore
+            current_state[row_index][col_index] = current_state[row_index][col_index - 1] # type: ignore
+            current_state[row_index][col_index - 1] =  temp # type: ignore
+            
+            queue.append(copy.deepcopy(current_state))
+            temp = current_state[row_index][col_index] # type: ignore
+            current_state[row_index][col_index] = current_state[row_index][col_index - 1] # type: ignore
+            current_state[row_index][col_index - 1] = temp # type: ignore
+       
+        if down(current_state, row_index, col_index):
+            temp = current_state[row_index][col_index] # type: ignore
+            current_state[row_index][col_index] = current_state[row_index + 1][col_index] # type: ignore
+            current_state[row_index + 1][col_index] = temp # type: ignore
+            
+            queue.append(copy.deepcopy(current_state))
+            temp = current_state[row_index][col_index] # type: ignore
+            current_state[row_index][col_index] = current_state[row_index + 1][col_index] # type: ignore
+            current_state[row_index + 1][col_index] = temp # type: ignore
+        
+        if right(current_state, row_index, col_index):
+            temp = current_state[row_index][col_index] # type: ignore
+            current_state[row_index][col_index] = current_state[row_index][col_index + 1] # type: ignore
+            current_state[row_index][col_index + 1] = temp # type: ignore
+            
+            queue.append(copy.deepcopy(current_state))
+            temp = current_state[row_index][col_index] # type: ignore
+            current_state[row_index][col_index] = current_state[row_index][col_index + 1] # type: ignore
+            current_state[row_index][col_index + 1] = temp # type: ignore
+        i+=1
 
 create(g)
